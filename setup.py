b60505c7c8b3207430a14ece430b20964e77aae4
ymir <ymir@lubuntu64bitDev.home>
2017-12-07 11:24:55 +0100

    set version to 0.3
    file:///home/ymir/Development/HelloWorldPython3/helloworldpython3/HelloWorldPython3.py
    file:///home/ymir/Development/HelloWorldPython3/helloworldpython3/__init__.py
    file:///home/ymir/Development/HelloWorldPython3/setup.py
    file:///home/ymir/Development/HelloWorldPython3/helloworldpython3/HelloWorldPython3.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+
+import setuptools
+
+module_path = os.path.join(os.path.dirname(__file__), 'helloworldpython3/HelloWorldPython3.py')
+version_line = [line for line in open(module_path)
+                if line.startswith('__version__')][0]
+
+__version__ = version_line.split('__version__ = ')[-1][1:][:-2]
+
+setuptools.setup(
+    name="HelloWorldPython3",
+    version=__version__,
+    description="Dummy test for Python3",
+    py_modules=['helloworldpython3.HelloWorldPython3'],
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
--- src/HelloWorldPython3.py
+++
@@ -1,1 +0,0 @@
-print("If you can read this message, Python3 works fine")
